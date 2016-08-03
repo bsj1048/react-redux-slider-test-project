0308abd49b7b759a081474888dcdb67e82966e97
Maxim Smotrov <msmotrov.ctr@thehonestcompany.com>
2016-08-03 18:33:21 +0300

    Removed unused packages and added spread operator in place of Object.assign

--- src/reducers/sliderReducer.js
+++ src/reducers/sliderReducer.js
@@ -3,9 +3,9 @@ import * as types from '../actions/actionTypes';
 const sliderReducer = (state, action) => {
     switch(action.type) {
         case types.SLIDE:
-            return Object.assign({}, state, { currentSlideIndex: action.nextSlideIndex });
+            return { ...state, currentSlideIndex: action.nextSlideIndex };
         case types.SAVE_AUTOSLIDE_TIMER:
-            return Object.assign({}, state, { changeSlideIntervalId: action.intervalId });
+            return { ...state, changeSlideIntervalId: action.intervalId };
         default:
             return state;
     }
